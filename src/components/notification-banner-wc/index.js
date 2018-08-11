0ae7659dea9621ba3eff46c430df87e0ff51ca5e
Paul Borrego <paul.borrego@turner.com>
2018-08-11 16:46:12 -0400

    feat(component): Using vanilla javascript
    Removed the polymer specific parts and just rolling with pure
    vanilla javascript.

--- src/components/notification-banner-wc/index.js
+++ src/components/notification-banner-wc/index.js
@@ -1,32 +1,82 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-
 /**
  * `notification-banner-wc`
  * A notification banner
  *
  * @customElement
- * @polymer
  * @demo demo/index.html
  */
-class NotificationBannerWc extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          display: block;
+class NotificationBannerWc extends HTMLElement {
+
+    static get template() {
+
+        return `
+            <style>
+                :host {
+                  display: block;
+                }
+            </style>
+            <h2></h2>
+        `;
+    }
+
+    static get observedAttributes() {
+
+        return ['prop1'];
+    }
+
+    constructor() {
+
+        super();
+
+        this.theRoot = this.attachShadow({mode: 'open'});
+        this.theRoot.innerHTML = NotificationBannerWc.template;
+    }
+
+    connectedCallback() {
+
+        this.render({prop1: this.prop1});
+    }
+
+    attributeChangedAttribute(prop, oldValue, newValue) {
+
+        if (oldValue !== newValue) {
+
+            switch (prop) {
+
+            case 'prop1':
+                this.prop1(newValue);
+                break;
+            }
         }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
-    `;
-  }
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'notification-banner-wc',
-      },
-    };
-  }
+    }
+
+    get prop1() {
+
+        return this.getAttribute('prop1') || 'notification-banner-wc';
+    }
+
+    set prop1(nextValue) {
+
+        this.setAttribute('prop1', nextValue);
+        this.render({prop1: nextValue});
+    }
+
+    render({ prop1 }) {
+
+        const $h2 = this.theRoot.querySelector('h2');
+        const $nextH2 = document.createDocumentFragment();
+
+        $nextH2.textContent = `Hello ${prop1}!`;
+
+        while ($h2.firstChild) {
+
+            $h2.removeChild($h2.firstChild);
+        }
+
+        $h2.appendChild($nextH2); 
+    }
 }
+
+
 
 window.customElements.define('notification-banner-wc', NotificationBannerWc);
